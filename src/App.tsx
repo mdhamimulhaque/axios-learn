0a24a1efdab8a4ff326d955845dde673ae972507
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-02-20 13:41:38 +0600

    fetch data from server [done]

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddPost from "./components/AddPost";
 import EditPost from "./components/EditPost";
@@ -8,7 +9,7 @@ import { PostType } from "./types/PostTypes";
 export default function App() {
   const [posts, setPosts] = useState<PostType[] | []>([]);
   const [post, setPost] = useState<PostType | null>(null); // post I am editing
-
+  const [error, setError] = useState<string | null>(null);
   const handleAddPost = (newPost) => {
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
 
@@ -38,6 +39,26 @@ export default function App() {
     setPosts(updatedPosts);
   };
 
+  // ---> fetch data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/posts");
+        if (response && response?.data) {
+          setPosts(response?.data);
+        }
+      } catch (err) {
+        if (err.response) {
+          // error came from server
+          setError(
+            `Error from server: status: ${err.response.status} - message: ${err.response.statusText}`
+          );
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div>
@@ -59,6 +80,13 @@ export default function App() {
             <EditPost post={post} onEditPost={handleEditPost} />
           )}
         </div>
+
+        {error && (
+          <>
+            <hr />
+            <div className="error">{error}</div>
+          </>
+        )}
       </div>
     </div>
   );
